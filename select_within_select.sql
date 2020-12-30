edba09b99e9546c7f1814f055140c87064d54faa
John Grinstead <john.allen.grinstead@gmail.com>
2020-12-29 20:05:43 -0600

    completed select within select exercise

--- select_within_select.sql
+++ select_within_select.sql
@@ -35,4 +35,62 @@ and population <
     from world where 
     name = 'Poland');
 
-    
+-- Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+
+SELECT name,
+   CONCAT(
+       ROUND(
+           100*population/(
+               SELECT population 
+               FROM world 
+               WHERE name='Germany')
+            ),
+        '%')as percentage
+FROM world
+WHERE continent='Europe';
+
+-- Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+
+select name
+from world
+where gdp  >= all(
+    select gdp 
+    from world 
+    where continent ='Europe' and gdp >0) 
+    and continent != 'Europe';
+
+-- Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area >0);
+
+-- List each continent and the name of the country that comes first alphabetically.
+
+SELECT continent,name FROM world x
+  WHERE x.name <= ALL (
+    SELECT name FROM world y
+     WHERE x.continent=y.continent);
+
+-- Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+
+SELECT name, continent, population 
+FROM world w
+WHERE NOT EXISTS (                  
+   FROM world nx
+   WHERE nx.continent = w.continent 
+   AND nx.population > 25000000     
+   );
+
+-- Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+
+SELECT x.name, x.continent
+FROM world AS x
+WHERE x.population/3 > ALL (
+  SELECT y.population
+  FROM world AS y
+  WHERE x.continent = y.continent
+  AND x.name != y.name);
+
